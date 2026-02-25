8c4138602689b46fbb00f062af2994eb6739964a
Ranjithsiji <ranjith.sajeev@gmail.com>
2026-02-25 23:42:41 +0530

    Update working directory

---
+++ test_ref.js
@@ -0,0 +1,10 @@
+console.log(JSON.stringify({
+    "P854": [{
+        "snaktype": "value",
+        "property": "P854",
+        "datavalue": {
+            "type": "string",
+            "value": "https://example.com"
+        }
+    }]
+}))
